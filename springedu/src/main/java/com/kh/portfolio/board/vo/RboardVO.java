3cbf0f46399cf2451c9be8e93e5cba6af3dba331
sewon jang <dkrlrhdrur@gmail.com>
2020-08-21 15:33:49 +0900

    1. 댓글 작성 백앤드 구현: controller 2. 예외처리: GlobalExceptionHandler.java 					 RestExceptionHandler.java 		예외 클래스: RestEAccessException.java 		예외처리뷰	: exception.jsp

--- springedu/src/main/java/com/kh/portfolio/board/vo/RboardVO.java
+++ springedu/src/main/java/com/kh/portfolio/board/vo/RboardVO.java
@@ -3,6 +3,10 @@ package com.kh.portfolio.board.vo;
 import java.sql.Timestamp;
 
 import javax.persistence.Entity;
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Positive;
+import javax.validation.constraints.Size;
 
 import lombok.Data;
 
@@ -10,12 +14,27 @@ import lombok.Data;
 @Data
 public class RboardVO {
 
-private long rnum;				//	RNUM	NUMBER(10,0)	No		1	"댓글 번호
-private long bnum;				//	BNUM	NUMBER(10,0)	No		2	"게시글 번호
+@NotNull
+@Positive
+private Long rnum;				//	RNUM	NUMBER(10,0)	No		1	"댓글 번호
+
+@NotNull
+@Positive
+private Long bnum;				//	BNUM	NUMBER(10,0)	No		2	"게시글 번호
+
+@NotNull
+@Email
+//@Pattern(regexp="\\w+@\\w.\\w+(\\.\\w+)?", message="이메일 형식으로 입력바랍니다.")
 private String rid;				//	RID	VARCHAR2(40 BYTE)	No		3	"작성자 ID
+
+@NotNull
+@Size(min=2, max=10, message="별칭은 최대 10자까지 가능합니다.")
 private String rnickname;	//	RNICKNAME	VARCHAR2(30 BYTE)	Yes		4	작성자이름(별칭)
+
 private Timestamp rcdate;			//	RCDATE	TIMESTAMP(6)	No	SYSTIMESTAMP 	5	작성일
 private Timestamp rudate;			//	RUDATE	TIMESTAMP(6)	Yes	SYSTIMESTAMP 	6	수정일
+
+@NotNull
 private String rcontent;		//	RCONTENT	CLOB	No		7	본문내용
 
 //선호도
@@ -28,7 +47,7 @@ private int rstep;				//	RSTEP	NUMBER(5,0)	Yes		11	답변글의단계
 private int rindent;			//	RINDENT	NUMBER(5,0)	Yes		12	답변글의들여쓰기
 
 //부모 댓글 참조
-private long prnum;				//	PRNUM	NUMBER(10,0)	Yes		13	부모댓글번호
+private Long prnum;				//	PRNUM	NUMBER(10,0)	Yes		13	부모댓글번호
 private String prid;			//	PRID	VARCHAR2(40 BYTE)	Yes		14	부모댓글아이디
 private String prnickname;//	PRNICKNAME	VARCHAR2(30 BYTE)	Yes		15	부모댓글작성자(별칭)
 
--- springedu/src/main/java/com/kh/portfolio/board/vo/VoteVO.java
+++ springedu/src/main/java/com/kh/portfolio/board/vo/VoteVO.java
@@ -1,12 +1,20 @@
 package com.kh.portfolio.board.vo;
 
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotNull;
+
 import lombok.Data;
 
 @Data
 public class VoteVO {
+@NotNull
 private long rnum;			//	RNUM	NUMBER(10,0)	No		1	"댓글번호
+@NotNull
 private long bnum;			//	BNUM	NUMBER(10,0)	No		2	"게시글 번호
+@NotNull
+@Email
 private String rid;			//	RID	VARCHAR2(40 BYTE)	No		3	"투표자(회원ID)
+@NotNull
 private Vote vote;			//VOTE	CHAR(1 BYTE)	No		4	1:호감 2:비호감
 
 }
